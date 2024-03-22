31ed276fa6a86c17d6fff174cfbdd757ab46ade3
Akira Hayashi <smxshxishxad@me.com>
2024-03-23 03:29:10 +0900

    Add test for `App.IsValid()`

--- application/types_test.go
+++ application/types_test.go
@@ -41,3 +41,29 @@ func TestAppIsDefaultBranch(t *testing.T) {
 		assert.False(t, app.IsDefaultBranch(event))
 	})
 }
+
+func TestAppIsValid(t *testing.T) {
+	app := New("required-label", "main")
+
+	t.Run("Valid", func(t *testing.T) {
+		event := domain.ParsedEvent{
+			Branch: "main",
+			Labels: domain.Labels{"required-label"},
+		}
+		assert.True(t, app.IsValid(event))
+	})
+	t.Run("NoLabel", func(t *testing.T) {
+		event := domain.ParsedEvent{
+			Branch: "main",
+			Labels: domain.Labels{},
+		}
+		assert.False(t, app.IsValid(event))
+	})
+	t.Run("NotDefaultBranch", func(t *testing.T) {
+		event := domain.ParsedEvent{
+			Branch: "other-branch",
+			Labels: domain.Labels{"required-label"},
+		}
+		assert.False(t, app.IsValid(event))
+	})
+}
