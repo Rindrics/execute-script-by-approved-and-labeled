7bc9c9f5d47cfe67175d2653752cf9ce01b2767c
Akira Hayashi <smxshxishxad@me.com>
2024-03-24 07:36:39 +0900

    Implement `ParseGitHubEvent()` in package `infrastructure`

---
+++ infrastructure/parser.go
@@ -0,0 +1,41 @@
+package infrastructure
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/google/go-github/github"
+)
+
+type EventParser struct{}
+
+func ParseGitHubEvent() (*github.PullRequestEvent, error) {
+	eventPath := os.Getenv("GITHUB_EVENT_PATH")
+	if eventPath == "" {
+		return nil, fmt.Errorf("GITHUB_EVENT_PATH environment variable not set")
+	}
+
+	data, err := ioutil.ReadFile(eventPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the data into a generic map
+	var genericData map[string]interface{}
+	if err := json.Unmarshal(data, &genericData); err != nil {
+		return nil, err
+	}
+
+	// Judge the type of the event
+	if _, ok := genericData["pull_request"]; ok {
+		var event github.PullRequestEvent
+		if err := json.Unmarshal(data, &event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	} else {
+		return nil, fmt.Errorf("unsupported or unknown event type in the provided data")
+	}
+}
